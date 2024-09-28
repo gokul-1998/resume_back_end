cf1eba3fc958c6b4ae6d5612711408cd2fbff4ce
itsranjithkumar <163500531+itsranjithkumar@users.noreply.github.com>
2024-09-28 12:44:42 +0530

    bug

--- back_end/app/main.py
+++ back_end/app/main.py
@@ -6,6 +6,11 @@ from .routers import users, resumes, auth  # Import the routers
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",  # Your frontend origin
+    "http://127.0.0.1:8000",  # Alternative frontend origin
+]
+
 # Create all the database tables
 models.Base.metadata.create_all(bind=engine)
 
